b4ade2223ac843612d379812e721d456583c5b59
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2025-05-17 00:03:45 +0300

    Add DepthStencil

--- source/renderer/device.cpp
+++ source/renderer/device.cpp
@@ -15,6 +15,8 @@ static ComPtr<ID3D11Buffer> g_indexBuffer;
 static ComPtr<ID3D11VertexShader> g_vertexShader;
 static ComPtr<ID3D11PixelShader> g_pixelShader;
 static ComPtr<ID3D11InputLayout> g_inputLayout;
+static ComPtr<ID3D11Texture2D> g_depthStencilBuffer;
+static ComPtr<ID3D11DepthStencilView> g_depthStencilView;
 
 struct Vertex {
     DirectX::XMFLOAT3 position;
@@ -77,13 +79,14 @@ bool ashenvale::renderer::device::initialize()
     if (FAILED(result))
         return false;
 
-    DXGI_MODE_DESC1* displayModeList = new DXGI_MODE_DESC1[numModes];
+    DXGI_MODE_DESC1 displayModeList[512] = {};
     result = g_baseOutput->GetDisplayModeList1(
         format,
         0,
         &numModes,
         displayModeList
     );
+
     if (FAILED(result))
         return false;
 
@@ -99,8 +102,6 @@ bool ashenvale::renderer::device::initialize()
         numerator = 60;
         denominator = 1;
     }
-
-    delete[] displayModeList;
 
     DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
     swapChainDesc.BufferCount = 2;
@@ -190,6 +191,33 @@ bool ashenvale::renderer::device::initialize()
         return false;
     }
 
+    D3D11_TEXTURE2D_DESC depthDesc = {};
+    depthDesc.Width = 1280;
+    depthDesc.Height = 720;
+    depthDesc.MipLevels = 1;
+    depthDesc.ArraySize = 1;
+    depthDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT; // 24 bit depth + 8 bit stencil
+    depthDesc.SampleDesc.Count = 1;                   // MSAA yoksa 1
+    depthDesc.SampleDesc.Quality = 0;
+    depthDesc.Usage = D3D11_USAGE_DEFAULT;
+    depthDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+    
+
+    g_device->CreateTexture2D(&depthDesc, nullptr, g_depthStencilBuffer.GetAddressOf());
+    if (FAILED(result)) {
+        return false;
+    }
+
+    D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
+    dsvDesc.Format = depthDesc.Format;
+    dsvDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+    dsvDesc.Texture2D.MipSlice = 0;
+
+    result = g_device->CreateDepthStencilView(g_depthStencilBuffer.Get(), &dsvDesc, g_depthStencilView.GetAddressOf());
+    if (FAILED(result)) {
+        return false;
+    }
+
     UINT stride = sizeof(Vertex);
     UINT offset = 0;
     g_context->IASetVertexBuffers(0, 1, g_vertexBuffer.GetAddressOf(), &stride, &offset);
@@ -238,7 +266,9 @@ void ashenvale::renderer::device::render()
     static ComPtr<ID3D11RenderTargetView> rtv;
     renderer::device::g_renderTargetView.As(&rtv);
 
-    g_context->OMSetRenderTargets(1, rtv.GetAddressOf(), nullptr);
+    g_context->OMSetRenderTargets(1, rtv.GetAddressOf(), g_depthStencilView.Get());
+    g_context->ClearDepthStencilView(g_depthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
 
     const float color[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
     g_context->ClearRenderTargetView(g_renderTargetView.Get(), color);
