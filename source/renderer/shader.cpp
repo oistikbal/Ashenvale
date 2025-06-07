c51f5329fc9a1c84f8533053a1ffd04de8818e11
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2025-06-07 10:58:13 +0300

    Add shader.cpp

---
+++ source/renderer/shader.cpp
@@ -0,0 +1,73 @@
+#include "shader.h"
+#include "renderer/device.h"
+#include "shader_compiler.h"
+#include <d3d11_4.h>
+#include <winrt/base.h>
+
+using namespace winrt;
+
+namespace
+{
+void compile(ashenvale::renderer::shader::shader &shader, const wchar_t *vsPath, const wchar_t *psPath)
+{
+    com_ptr<ID3DBlob> errorBlob;
+    com_ptr<ID3DBlob> vsBlob;
+    com_ptr<ID3DBlob> psBlob;
+
+    ashenvale::renderer::shader_compiler::compile(vsPath, "main", "vs_5_0", nullptr, vsBlob.put(), errorBlob.put());
+
+    ashenvale::renderer::device::g_device->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(),
+                                                              nullptr, shader.vertexShader.put());
+
+    ashenvale::renderer::shader_compiler::compile(psPath, "main", "ps_5_0", nullptr, psBlob.put(), errorBlob.put());
+
+    ashenvale::renderer::device::g_device->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(),
+                                                             nullptr, shader.pixelShader.put());
+
+    com_ptr<ID3D11ShaderReflection> reflection;
+    D3DReflect(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), IID_PPV_ARGS(reflection.put()));
+
+    auto layouts = ashenvale::renderer::shader_compiler::get_input_layout(reflection.get());
+
+    ashenvale::renderer::device::g_device->CreateInputLayout(layouts.data(), layouts.size(), vsBlob->GetBufferPointer(),
+                                                             vsBlob->GetBufferSize(), shader.inputLayout.put());
+
+    D3D11_SHADER_DESC shaderDesc = {};
+    reflection->GetDesc(&shaderDesc);
+
+    for (UINT i = 0; i < shaderDesc.BoundResources; ++i)
+    {
+        D3D11_SHADER_INPUT_BIND_DESC desc = {};
+        if (SUCCEEDED(reflection->GetResourceBindingDesc(i, &desc)) && desc.Name)
+        {
+            ashenvale::renderer::shader::resource_binding binding;
+            binding.name = desc.Name;
+            binding.slot = desc.BindPoint;
+            binding.bindCount = desc.BindCount;
+            binding.type = desc.Type;
+            shader.vsBindings.push_back(binding);
+        }
+    }
+
+    D3DReflect(psBlob->GetBufferPointer(), psBlob->GetBufferSize(), IID_PPV_ARGS(reflection.put()));
+    for (UINT i = 0; i < shaderDesc.BoundResources; ++i)
+    {
+        D3D11_SHADER_INPUT_BIND_DESC desc = {};
+        if (SUCCEEDED(reflection->GetResourceBindingDesc(i, &desc)) && desc.Name)
+        {
+            ashenvale::renderer::shader::resource_binding binding;
+            binding.name = desc.Name;
+            binding.slot = desc.BindPoint;
+            binding.bindCount = desc.BindCount;
+            binding.type = desc.Type;
+            shader.psBindings.push_back(binding);
+        }
+    }
+}
+} // namespace
+
+void ashenvale::renderer::shader::initialize()
+{
+    compile(g_triangleShader, L"vs.hlsl", L"ps.hlsl");
+    compile(g_quadShader, L"quad_vs.hlsl", L"quad_ps.hlsl");
+}
---
+++ source/renderer/shader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <d3d11_4.h>
+#include <string>
+#include <winrt/base.h>
+
+namespace ashenvale::renderer::shader
+{
+struct resource_binding
+{
+    std::string name;
+    D3D_SHADER_INPUT_TYPE type;
+    uint32_t slot;
+    uint32_t bindCount;
+};
+
+struct shader
+{
+    winrt::com_ptr<ID3D11VertexShader> vertexShader;
+    winrt::com_ptr<ID3D11PixelShader> pixelShader;
+    winrt::com_ptr<ID3D11InputLayout> inputLayout;
+
+    std::vector<resource_binding> vsBindings;
+    std::vector<resource_binding> psBindings;
+};
+
+inline shader g_triangleShader;
+inline shader g_quadShader;
+} // namespace ashenvale::renderer::shader
+
+namespace ashenvale::renderer::shader
+{
+void initialize();
+}
